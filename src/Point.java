8e49a7ce82bab38e127173045f0e84ebb453d58b
Angel Chen <angelc470@nycstudents.net>
2022-10-04 08:43:00 -0400

    Initial commit

---
+++ src/Point.java
@@ -0,0 +1,54 @@
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public Point(int one){
+        x = one;
+        y = one;
+    }
+    public Point(){
+        x = 0;
+        y = 0;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    public String coordinate() {
+        return"(" + x + ", " + y + ")";
+    }
+    public String quadrant(){
+        String quadrant;
+        if(x>0 && y>0){
+            quadrant = "I";
+        }else if(x<0 && y>0){
+            quadrant = "II";
+        }else if(x<0 && y<0){
+            quadrant = "III";
+        }else if(x>0 && y<0){
+            quadrant = "IV";
+        }else if (x==0 && y==0){
+            quadrant = "origin";
+        }else{
+            quadrant = "on an axis";
+        }
+        return quadrant;
+    }
+
+}
